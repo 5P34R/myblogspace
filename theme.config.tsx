8f7883f2ab1cc1fc1fec29ef0edfcdf7e1055a21
VishDroid-dev <vk2345732@gmail.com>
2023-07-09 22:38:21 +0530

    added trabodactf crypto challenges

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,8 +5,7 @@ const config: DocsThemeConfig = {
   logo: <span>5P34R writeups</span>,
   project: {
     link: 'https://github.com/VishDroid-dev',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  },  
   footer: {
     text: 'Made with love by VishDroid-dev 💗',
   },
